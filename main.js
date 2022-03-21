0e83f896d18fd04716943d90140eccd1a51917a5
Cris <mirocristy@gmail.com>
2022-03-21 11:47:53 +0100

    segundo commit

---
+++ main.js
@@ -0,0 +1,25 @@
+document.write('1. Crear una cadena de texto y reemplazar una de sus palabras. ')
+document.write('<br>');
+document.write('<br>');
+let aprender = '- Estoy aprendiendo CSS. ';
+document.write(aprender);
+document.write('<br>');
+document.write('<br>');
+document.write(aprender.replace('CSS', 'Javascript'));
+document.write('<br>');
+document.write('<br>');
+
+
+document.write('2. Crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas. ');
+document.write('<br>');
+document.write('<br>');
+
+let cadena = ' - Soy una cadena de texto.';
+document.write(cadena);
+document.write('<br>');
+document.write('<br>');
+document.write(cadena.toUpperCase());
+
+
+
+
